51cc782c0ae89c5d2cabcb6ed0edec1b7cc2c43c
Gian Ferreira <56806058+gianferreira@users.noreply.github.com>
2020-06-26 23:36:16 -0300

    : Cabeçalho da pagina de Agendametnos
    Estrutura e funcionalidades do Header da pagina de Agendamentos.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import GlobalStyle from './styles/global';
+import Routes from './routes';
 
 import AppProvider from './hooks';
 
-import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <Router>
